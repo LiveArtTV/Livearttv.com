514b6928a18b5ac4105266d1381776797bb21769
Vitali Makarov <405112@gmail.com>
2016-10-20 22:51:15 +0300

    bugfix: many js errors and update menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def store_block?
     ! %w(user_sessions users).include?(controller_name)
   end
+
+  def slider_block?
+    %w(home).include?(controller_name)
+  end
 end
