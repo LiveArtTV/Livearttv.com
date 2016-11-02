bda9c75b75ec2b1c56411fb9bfc0b9377f612aea
Vitali Makarov <405112@gmail.com>
2016-11-02 15:59:28 +0300

    feature: set mail sending through mandrill

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,7 @@ module Eliteshopping
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.action_mailer.sendmail_settings = { :arguments => '-i -t -f eliteshopping@sumatosoft.com' }
   end
 end
