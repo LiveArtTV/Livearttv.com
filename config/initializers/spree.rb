71d80a387c2aabed2f1558a8db2f425d7a4e0801
Vitali Makarov <405112@gmail.com>
2016-11-04 22:05:09 +0300

    feature: update logo #EL-6

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -21,7 +21,7 @@ Spree.config do |config|
   # Example:
   # Uncomment to stop tracking inventory levels in the application
   # config.track_inventory_levels = false
-  config.logo = 'logo.png'
+  config.logo = 'logo.svg'
   config.company_title   = 'Elite Shopping'
   config.company_address = 'Warrent House, 221b Baker Street - London - United Kingdom'
   config.company_phone   = '(+84). 123 456 789'
