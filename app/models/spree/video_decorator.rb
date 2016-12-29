006ece39a7f22f3a1a2934aea55036c66e4adf72
Vitali Makarov <405112@gmail.com>
2016-12-29 23:45:48 +0300

    bugfix: add validate youtube link format  #el-86

--- app/models/spree/video_decorator.rb
+++ app/models/spree/video_decorator.rb
@@ -2,4 +2,6 @@ Spree::Video.class_eval do
   attr_accessor :author_id
   belongs_to :author, -> {where spree_videos: {watchable_type: 'Spree::Author'}}, class_name: 'Author', foreign_key: :watchable_id
   scope :orderby, -> (opt) { order(opt.is_a?(Array) && opt[1] ? {opt[0] => opt[1]} : opt) }
+  validates :youtube_ref, format: {with: /(v=|\/)([\w-]+)(&.+)?$/, message: :incorrect_link, multiline: true}, if: Proc.new{|model| model.youtube_ref.present?}
+
 end
