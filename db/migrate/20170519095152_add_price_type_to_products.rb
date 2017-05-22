58de38bd1d0319bfee488c42ea8056648c9fe50f
Navarrskij <rostov-7@yandex.ru>
2017-05-22 13:54:45 +0300

    add migration for update all price type_

--- db/migrate/20170519095152_add_price_type_to_products.rb
+++ db/migrate/20170519095152_add_price_type_to_products.rb
@@ -1,7 +1,11 @@
 class AddPriceTypeToProducts < ActiveRecord::Migration
 
-  def up
-  	add_column :spree_products, :price_type, :integer
-  	Spree::Product.update_all(price_type: 0) # real_price = 0, not sale = 1, request_price = 2
+	def up
+		add_column :spree_products, :price_type, :integer
+		Spree::Product.update_all(price_type: 0) # real_price = 0, not sale = 1, request_price = 2
+  end
+
+  def down
+    remove_column :spree_products, :price_type
   end
 end
