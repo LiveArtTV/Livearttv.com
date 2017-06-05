db2a9b39231bf4338a6117088885f17cdf856949
Navarrskij <rostov-7@yandex.ru>
2017-06-05 14:58:42 +0300

    fix tabs

--- app/models/spree/price_decorator.rb
+++ app/models/spree/price_decorator.rb
@@ -1,7 +1,7 @@
 Spree::Price.class_eval do
 
-	attr_writer :price_before_discount
- 	def price_before_discount
- 		amount + discount
- 	end
+  attr_writer :price_before_discount
+  def price_before_discount
+    amount + discount
+  end
 end
--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -1,4 +1,4 @@
 Spree::Variant.class_eval do
-	delegate_belongs_to :default_price, :discount
-	delegate_belongs_to :default_price, :price_before_discount
+  delegate_belongs_to :default_price, :discount
+  delegate_belongs_to :default_price, :price_before_discount
 end
