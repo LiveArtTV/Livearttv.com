0edbe9808ba9c290a625b65f1a383000d2a1b986
Alex <a.rymar@sumatosoft.com>
2017-06-13 14:45:18 +0300

    refactoring load price

--- app/controllers/spree/admin/products_controller_decorator.rb
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -1,35 +1,19 @@
 Spree::Admin::ProductsController.class_eval do
 
+  DATA_COLUMNS = [:name, {:author => [:name]}, :price, :price_type, :id, :created_at, :description, :description_short,
+  :available_on, :discontinue_on, :promotionable, :shipping_category_id, :tax_category_id, :meta_description,
+  :meta_keywords, :meta_title, :slug, :updated_at]
+
+  HEADER_COLUMNS = ['Name', 'Author', 'Price', 'Price_type', 'ID', 'Created', 'Description', 'Description_short',
+  'Available_on', 'Discontinue_on', 'Promotionable', 'shipping_category_id', 'tax_category_id', 'meta_description',
+   'meta_keywords', 'meta_title', 'slug', 'Updated_at']
+
   def index
     respond_to do |format|
       format.html { render :index }
       format.xls do
         @products = Spree::Product.all
-        send_data @products.to_xls(:columns =>
-          [
-          :name,
-          {:author => [:name]},
-          :price,
-          :price_type,
-          :id,
-          :created_at,
-          :description,
-          :description_short,
-          :available_on,
-          :discontinue_on,
-          :promotionable,
-          :shipping_category_id,
-          :tax_category_id,
-          :meta_description,
-          :meta_keywords,
-          :meta_title,
-          :slug,
-          :updated_at
-          ],
-        :headers => ['Name', 'Author', 'Price', 'Price_type', 'ID', 'Created', 'Description', 'Description_short',
-        'Available_on', 'Discontinue_on', 'Promotionable', 'shipping_category_id', 'tax_category_id', 'meta_description',
-         'meta_keywords', 'meta_title', 'slug', 'Updated_at']),
-        :filename => 'products.xls'
+        send_data @products.to_xls(:columns => DATA_COLUMNS, :headers => HEADER_COLUMNS), :filename => 'products.xls'
       end
     end
   end
