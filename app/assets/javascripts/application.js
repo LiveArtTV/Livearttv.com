e78424eae3514053b61db5ba4c8548b94d5f3e88
Alex <a.rymar@sumatosoft.com>
2017-07-27 18:21:39 +0300

    some change home page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -165,7 +165,7 @@ $(document).ready(function(){
 $(function() {
   $('.pop-up').hide();
   $('.pop-up').fadeIn(1000);
-  
+
   $('.close-button').click(function (e) {
     $('.pop-up').fadeOut(700);
     $('.pop-up_overlay').css({display:'none'});
