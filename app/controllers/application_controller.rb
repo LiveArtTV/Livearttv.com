9815652965961581d5a964e8f3610079f8fd4524
Alex <a.rymar@sumatosoft.com>
2017-07-28 18:52:36 +0300

    close home page production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  #before_action :redirect_to_landing
+  before_action :redirect_to_landing
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include Globals::ErrorPages
